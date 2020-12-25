69d5a13632fd1e8c999e4bf28fad3c15f05bd4ed
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-12-25 23:33:06 +0200

    fix

---
+++ precache-manifest.5c793167a993f40d6e86ac7476096586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803526559c75bdb24081",
+    "url": "/css/app.30337d21.css"
+  },
+  {
+    "revision": "84ba12eaad57de18f068",
+    "url": "/css/chunk-vendors.df61abdf.css"
+  },
+  {
+    "revision": "9d244db415a9d3ca51e51d784b1f0987",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803526559c75bdb24081",
+    "url": "/js/app.fa2745e0.js"
+  },
+  {
+    "revision": "84ba12eaad57de18f068",
+    "url": "/js/chunk-vendors.661f7b85.js"
+  },
+  {
+    "revision": "ddb6d62fd70519895103e8e1a3ebfd48",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
